72e601a1dce36163b2f59fa9c4ac7013e0f63bbc
Kennard <kennard.mcgill@gatech.edu>
2024-11-18 10:56:17 -0500

    JUnit backend test updated

--- booksearch/src/test/java/com/library/booksearch/BooksearchApplicationTests.java
+++ booksearch/src/test/java/com/library/booksearch/BooksearchApplicationTests.java
@@ -1,13 +1,47 @@
 package com.library.booksearch;
 
+import com.library.booksearch.service.book.BookService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class BooksearchApplicationTests {
 
+	@Autowired
+	private BookService bookService;
+
+	// Testing Service Implementation. We have 5 services which are tested here for a response from the api call
+
 	@Test
-	void contextLoads() {
+	void testTitles() {
+		String resp = bookService.getReqTitle("react");
+		assertNotNull(resp);
+	}
+
+	@Test
+	void testQueryParamTitles() {
+		String resp = bookService.getTitleQueryParam("debug");
+		assertNotNull(resp);
+	}
+	@Test
+	void testSearchAuthors() {
+		String resp = bookService.getReqAuthor("twain");
+		assertNotNull(resp);
+	}
+
+	@Test
+	void testSearchSubjects() {
+		String resp = bookService.getReqSubject("sharks");
+		assertNotNull(resp);
+	}
+
+	@Test
+	void testTitlesPages() {
+		String resp = bookService.getReqTitlePages("debug", 4);
+		assertNotNull(resp);
 	}
 
 }
+
