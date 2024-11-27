e28ee413b46b093b26ab71e6f066dcbc2faf465b
Robert Daniel <rd842003@gmail.com>
2024-11-26 21:24:34 -0500

    About us Page updated

--- booksearch/src/main/react/containers/AboutUsPage.jsx
+++ booksearch/src/main/react/containers/AboutUsPage.jsx
@@ -3,7 +3,13 @@ import "./style.css";
 
 const AboutUsPage = () => {
 return (
-    <div>About us page content</div>
+    <div>Lorem ipsum odor amet, consectetuer adipiscing elit. 
+        Iaculis nostra nibh pellentesque est laoreet curae pulvinar. 
+        Id amet morbi laoreet, augue vel mauris dolor. 
+        Rhoncus sociosqu vestibulum enim dolor orci efficitur neque. 
+        Porttitor aenean ipsum congue cras aptent netus rutrum. 
+        Vestibulum condimentum class dis lacinia ex phasellus, ipsum nec vestibulum? 
+        Commodo phasellus at natoque arcu tellus, amet maecenas.</div>
     )
 }
 export default AboutUsPage;
